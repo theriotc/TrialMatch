3d2501bd9f0e78456dc6deaaefa1761ccf1cd76e
theriotc <christopher.theriot@gmail.com>
2025-08-06 13:55:00 -0400

    undo debug setting

--- backend/app.py
+++ backend/app.py
@@ -318,4 +318,4 @@ def summarize_trial_results(primary_outcomes):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port) 
+    app.run(debug=False, host='0.0.0.0', port=port) 
