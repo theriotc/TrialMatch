13b4146bfe42834c6a7feb778051be9a0897bd7f
theriotc <christopher.theriot@gmail.com>
2025-08-06 01:06:46 -0400

    remove ssr for static upload

--- frontend/trial-match-app/src/app/app.config.ts
+++ frontend/trial-match-app/src/app/app.config.ts
@@ -1,12 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch())
   ]
 };
